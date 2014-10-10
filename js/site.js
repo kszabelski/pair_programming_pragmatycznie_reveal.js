0eb552d68a6a68e59675c49121b4b406f5d141c8
Krzysztof Szabelski <kszabelski@gmail.com>
2014-10-10 17:43:13 +0200

    Handle arrow on agenda

--- js/site.js
+++ js/site.js
@@ -1,12 +1,23 @@
 $(".document").ready(function(){
+
+	var emphasizeCurentElement = function (agendaTemplate, emphasizeElementClass){
+		agendaTemplate.prop({ id: "agenda" + emphasizeElementClass});
+		var emphasizeElement = $("." + emphasizeElementClass, agendaTemplate).addClass("emphasizedElement");
+	}
+
+	var setArrowPosition = function(agendaTemplate, arrowPosition){
+		if (arrowPosition)
+			$(".agendaArrow", agendaTemplate).css({ top: arrowPosition, left: "-40px" });
+		else
+			$(".agendaArrow", agendaTemplate).hide();
+	}
+	
 	$(".agendaPlaceholder").each(function(index, agendaPlaceholder)
 	{
-		var emphasizeElement = $(agendaPlaceholder).attr("data-emphasize-element");
-
 		var agendaTemplate = $("#agendaTemplate").clone();
-		agendaTemplate.prop({ id: "agenda" + emphasizeElement});
-
-		$("." + emphasizeElement, agendaTemplate).addClass("emphasizedElement");
+		emphasizeCurentElement(agendaTemplate, $(agendaPlaceholder).attr("data-emphasize-element"));
 		agendaTemplate.appendTo(agendaPlaceholder).show();
+
+		setArrowPosition(agendaTemplate, $(agendaPlaceholder).attr("data-arrow-top"));
 	})
 })
